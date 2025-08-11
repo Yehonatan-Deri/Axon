7d66ea1b715b5af5d2a6d9c2d7b8ffacf1810bc9
Yehonatan Deri <yonatander@gmail.com>
2025-08-11 21:32:30 +0300

    removes daemon=True and joins in a clearer order

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ if __name__ == "__main__":
     q_results = mp.Queue(maxsize=64)
 
     video_path = "People - 6387.mp4"
+    # video_path = "Lambs Running.mp4"
 
-    p_stream = mp.Process(target=streamer, args=(video_path, q_frames), daemon=True)
-    p_detect = mp.Process(target=detector, args=(q_frames, q_results), daemon=True)
-    p_show   = mp.Process(target=display,  args=(q_results,), daemon=True)
+    p_stream = mp.Process(target=streamer, args=(video_path, q_frames))
+    p_detect = mp.Process(target=detector, args=(q_frames, q_results))
+    p_show   = mp.Process(target=display,  args=(q_results,))
 
     try:
         p_stream.start()
@@ -23,6 +24,12 @@ if __name__ == "__main__":
 
         # Wait for display to finish (it will close on sentinel or ESC)
         p_show.join()
+
+        # After display ends, detector should already have exited after passing SENTINEL
+        p_detect.join(timeout=2)
+
+        # Streamer should have finished as well
+        p_stream.join(timeout=2)
 
     except KeyboardInterrupt:
         pass
